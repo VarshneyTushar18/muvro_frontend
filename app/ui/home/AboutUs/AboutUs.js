fc1481f3fb13481f3b1122d311a63370761a053f
tarunwebdesign22 <ts.tech2globe@gmail.com>
2026-04-22 16:55:15 +0530

    update

--- app/ui/home/AboutUs/AboutUs.js
+++ app/ui/home/AboutUs/AboutUs.js
@@ -25,7 +25,7 @@ export default function AboutUs() {
           <div className="row mt-4 justify-content-between">
             {/* Left Column (Heading) */}
             <div
-              className="col-xl-6 col-xxl-7"
+              className="col-xl-6 col-xxl-6"
               data-aos="fade-right"
               data-aos-delay="100"
             >
@@ -37,17 +37,16 @@ export default function AboutUs() {
 
             {/* Right Column (Content + Button) */}
             <div
-              className="col-lg-5 col-xl-5"
+              className="col-lg-6 col-xl-6"
               data-aos="fade-left"
               data-aos-delay="200"
             >
               <p>
-                Most of the technologies we use in our solutions are fully
-                designed, developed, manufactured in its world class factory and
-                installed & serviced by our teams only. This gives us
-                competitive advantage, quick delivery and a long-lasting
-                solution to our customers. We believe in flexibility and
-                modularity which reflect in our solutions.
+                Muvro Technologies is a global leader in warehouse automation and intralogistics, delivering futuristic, innovative, and modular solutions for fulfilment centres, distribution centres, last-mile delivery stations, dark stores, and quick commerce.<br/><br/>
+                With 23+ years of expertise, in-house manufacturing, and a global support network, we empower industries including e-commerce, parcels & postal, 3PL, retail, FMCG, automotive, and more to move smarter, faster, and more efficiently.<br/><br/>
+                Our portfolio spans high- High-Speed Cross-Belt Sorters, Linear Sorters, Swivel Wheel Sorters,3D Smart Sorters, ASRS Systems (Pallet ASRS & Bin-to-Person ASRS), Smart & High-Speed Conveying Systems, Electrical Control Systems and advanced automation software. <br/><br/>
+At Muvro, our mission is clear — to help businesses optimize operations, enhance efficiency, and gain a sustainable competitive edge in an ever-evolving market.
+
               </p>
 
               {/* Button to open modal */}
